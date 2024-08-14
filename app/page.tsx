ba8d27c1e96fb1e966ee1bdcb79368017164a2da
istredo <uvarov6799@gmail.com>
2024-08-14 19:59:37 +0300

    feat: added categories list

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { Container, Filters, Title, TopBar } from "@/components/shared";
+import { Container, Filters, ProductsList, Title, TopBar } from "@/components/shared";
+import { ProductCard } from "@/components/shared/product-card";
 
 import Image from "next/image";
 
@@ -21,6 +22,80 @@ export default function Home() {
 					<div className="flex-1">
 						<div className="flex flex-col gap-16">
 							Список товаров
+							<ProductsList title={"Мороженое"} items={[
+								{
+									id: 1,
+									name: "Фисташка",
+									imageUrl: 'https://br-delivery.ru/upload/iblock/1e8/b2gw05otqw3ghfw5u53kyqvfsej0v0xn.jpg',
+									price: 1549,
+									items: [{ price: 1549 }]
+								},
+								{
+									id: 2,
+									name: "Шоколадное",
+									imageUrl: 'https://br-delivery.ru/upload/iblock/ebc/nbqa7p66rv1j6674blwx31hkhsek60yr.jpg',
+									price: 1549,
+									items: [{ price: 1049 }]
+								},
+								{
+									id: 3,
+									name: "Бабл гам",
+									imageUrl: 'https://br-delivery.ru/upload/iblock/a41/ajs9niofoav2q4ijm2qar2jnrp0342mt.jpg',
+									price: 1549,
+									items: [{ price: 3549 }]
+								},
+								{
+									id: 4,
+									name: "Вишня",
+									imageUrl: 'https://br-delivery.ru/upload/iblock/1c7/3y1kvrdm1y4oxtwotl8cxvnvgh82q206.jpg',
+									price: 1549,
+									items: [{ price: 2549 }]
+								},
+								{
+									id: 5,
+									name: "Ванильное",
+									imageUrl: 'https://br-delivery.ru/upload/iblock/e0d/cfk5kutjupdl6of20ncksgbsk3ztt3sr.jpg',
+									price: 1549,
+									items: [{ price: 1549 }]
+								},
+							]} categoryId={0} />
+							<ProductsList title={"Торт-мороженое"} items={[
+								{
+									id: 1,
+									name: "Торт-мороженое",
+									imageUrl: 'https://brand-ice.ru/upload/iblock/fc8/fc884aa5791f4410ad7f1b52963b3bf4.jpg',
+									price: 1549,
+									items: [{ price: 1549 }]
+								},
+								{
+									id: 2,
+									name: "Торт-мороженка",
+									imageUrl: 'https://brand-ice.ru/upload/iblock/fc8/fc884aa5791f4410ad7f1b52963b3bf4.jpg',
+									price: 1549,
+									items: [{ price: 1049 }]
+								},
+								{
+									id: 3,
+									name: "Торт",
+									imageUrl: 'https://brand-ice.ru/upload/iblock/fc8/fc884aa5791f4410ad7f1b52963b3bf4.jpg',
+									price: 1549,
+									items: [{ price: 3549 }]
+								},
+								{
+									id: 4,
+									name: "Тортик",
+									imageUrl: 'https://brand-ice.ru/upload/iblock/fc8/fc884aa5791f4410ad7f1b52963b3bf4.jpg',
+									price: 1549,
+									items: [{ price: 2549 }]
+								},
+								{
+									id: 5,
+									name: "Торт-мороженое",
+									imageUrl: 'https://brand-ice.ru/upload/iblock/fc8/fc884aa5791f4410ad7f1b52963b3bf4.jpg',
+									price: 1549,
+									items: [{ price: 1549 }]
+								},
+							]} categoryId={1} />
 						</div>
 					</div>
 				</div>
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,20 +1,26 @@
+'use client'
 import React from 'react';
 import { cn } from '@/lib/utils';
+import { useCategoryStore } from '../store/category';
 
 interface Props {
 	className?: string;
 }
 
-const cats = ['Сливочное', 'Фруктовое', 'Мягкое', 'Фруктовый лёд']
-const activeIndex = 0
+const cats = [
+	{ id: 0, name: "Мороженое" },
+	{ id: 1, name: "Торт-мороженое" },
+	{ id: 2, name: "Мягкое" },
+	{ id: 3, name: "Фруктовый лёд" }
+]
 
 export const Categories: React.FC<Props> = ({ className }) => {
-
+	const activeCategoryId = useCategoryStore((state) => state.activeId)
 	return (
 		<div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
-			{cats.map((cat, index) => (
-				<a key={index} className={cn('flex items-center font-bold h-11 rounded-2xl px-5 text-nowrap', activeIndex === index && 'bg-white shadow-md shadow-gray-200 text-primary')}>
-					<button>{cat}</button>
+			{cats.map(({ name, id }, index) => (
+				<a key={index} href={`/#${name}`} className={cn('flex items-center font-bold h-11 rounded-2xl px-5 text-nowrap', activeCategoryId === id && 'bg-white shadow-md shadow-gray-200 text-primary')}>
+					<button>{name}</button>
 				</a>
 			))}
 		</div>
